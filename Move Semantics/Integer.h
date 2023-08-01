466999f0950037bde477da82ba48ed1dae4d1eb5
Van Hien Le <actionboyvn@gmail.com>
2023-08-01 11:13:55 +0200

    Added noexcept to move operations

--- Move Semantics/Integer.h
+++ Move Semantics/Integer.h
@@ -5,11 +5,11 @@ public:
 	Integer();
 	Integer(int iVal);
 	Integer(const Integer& pc_other);
-	Integer(Integer&& pc_other);
+	Integer(Integer&& pc_other) noexcept; // Move operations must not throw any exceptions
 	~Integer();
 
 	Integer& operator=(const Integer& pc_other);
-	Integer& operator=(Integer&& pc_other);
+	Integer& operator=(Integer&& pc_other) noexcept; // Move operations must not throw any exceptions
 	Integer operator+(const Integer& pc_other);
 	void vSetVal(int iVal);
 	int iGetVal() const;
